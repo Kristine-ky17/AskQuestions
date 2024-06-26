f526b099d95b27c2275ea8eb942f081695f2bafc
hi <120486409+Kristine-ky17@users.noreply.github.com>
2024-06-26 13:10:58 +0800

    Add files via upload

---
+++ main_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'AskQuestions!',page_icon = ':school:')
+st.sidebar.markdown(" Main Page🎈")
+st.title('Hey There! Welcome to AskQuestions! Sign in now and ask your question!')
